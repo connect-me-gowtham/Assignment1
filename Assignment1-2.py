6ae93fcf862db40dc8a9bfb2d12369a62ab3e180
connect-me-gowtham <connect.me.gowtham@gmail.com>
2025-06-01 12:49:01 +0530

    Update Assignment1-2.py

--- Assignment1-2.py
+++ Assignment1-2.py
@@ -1,3 +1,9 @@
+#Task 2: Create a Personalized Greeting
+#Problem Statement: Write a Python program that:
+#1.  Takes a user's first name and last name as input.
+#2.  Concatenates the first name and last name into a full name.
+#3.  Prints a personalized greeting message using the full name.
+
 First_Name=input("Enter your First Name: ")
 Last_Name=input("Enter you Last Name: ")
 print("Hello, " + First_Name +"",Last_Name+"! Welcome to the Python Program")
