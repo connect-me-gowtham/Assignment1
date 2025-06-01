c3a9462ac26a9c1be91693a62b93f41ca17b7cbf
connect-me-gowtham <connect.me.gowtham@gmail.com>
2025-06-01 12:43:05 +0530

    Add files via upload

---
+++ Assignment1-2.py
@@ -0,0 +1,3 @@
+First_Name=input("Enter your First Name: ")
+Last_Name=input("Enter you Last Name: ")
+print("Hello, " + First_Name +"",Last_Name+"! Welcome to the Python Program")
---
+++ Assignment1.py
@@ -0,0 +1,20 @@
+#Task 1: Perform Basic Mathematical Operations
+#Problem Statement: Write a Python program that does the following:
+#1.  Takes two numbers as input from the user.
+#2.  Performs the basic mathematical operations on these two numbers:
+#o	Addition
+#o	Subtraction
+#o	Multiplication
+#o	Division
+A = int(input("Enter the First Number: "))
+B = int(input("Enter the Second Number: "))
+Addition = (A+B)
+print("Addition:", Addition)
+Subtraction = (A-B)
+print("Subtraction:", Subtraction)
+Multiplication = (A*B)
+print("Multiplication:", Multiplication)
+Division = (A/B)
+print("Division:", Division)
+
+
